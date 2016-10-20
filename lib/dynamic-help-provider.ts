f1654dbf0e8c32f5312625f7c5ea330e5bf511b0
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-10-20 08:41:20 +0300

    Spike async-await. Use tns help command

--- lib/dynamic-help-provider.ts
+++ lib/dynamic-help-provider.ts
@@ -2,15 +2,15 @@ import * as constants from './constants';
 import Future = require("fibers/future");
 
 export class DynamicHelpProvider implements IDynamicHelpProvider {
-	public isProjectType(args: string[]): IFuture<boolean> {
-		return Future.fromResult(true);
+	public async isProjectType(args: string[]): Promise<boolean> {
+return true;
 	}
 
-	public getLocalVariables(options: { isHtml: boolean }): IFuture<IDictionary<any>> {
+	public async getLocalVariables(options: { isHtml: boolean }): Promise<IDictionary<any>> {
 		let localVariables: IDictionary<any> = {
 			constants: constants
 		};
-		return Future.fromResult(localVariables);
+		return localVariables;
 	}
 }
 $injector.register("dynamicHelpProvider", DynamicHelpProvider);
--- lib/nativescript-cli.ts
+++ lib/nativescript-cli.ts
@@ -3,14 +3,12 @@ let node = require("../package.json").engines.node;
 require("./common/verify-node-version").verifyNodeVersion(node, "NativeScript", "2.2.0");
 
 require("./bootstrap");
-import * as fiber from "fibers";
-import Future = require("fibers/future");
 import * as shelljs from "shelljs";
 shelljs.config.silent = true;
 import {installUncaughtExceptionListener} from "./common/errors";
 installUncaughtExceptionListener(process.exit);
 
-fiber(() => {
+async function init(): Promise<any> {
 	let config: Config.IConfig = $injector.resolve("$config");
 	let err: IErrors = $injector.resolve("$errors");
 	err.printCallStack = config.DEBUG;
@@ -21,11 +19,48 @@ fiber(() => {
 	messages.pathsToMessageJsonFiles = [/* Place client-specific json message file paths here */];
 
 	if (process.argv[2] === "completion") {
-		commandDispatcher.completeCommand().wait();
+		await commandDispatcher.completeCommand();
 	} else {
-		commandDispatcher.dispatchCommand().wait();
+		await commandDispatcher.dispatchCommand();
 	}
 
 	$injector.dispose();
-	Future.assertNoFutureLeftBehind();
-}).run();
+}
+
+init();
+
+
+
+
+// async function main() {
+//  await ping();
+// }
+
+// async function ping() {
+//  for (var i = 0; i < 10; i++) {
+//   try {
+//     await delay(300).catch(err => console.log("eeeeeeeeeeeee ", err));
+//   } catch (err) {
+//     console.log("error in await delay: ", err);
+//   }
+//   console.log("ping");
+//  }
+// }
+
+// function delay(ms: number) {
+//  return new Promise((resolve, reject) => setTimeout(reject, ms));
+// }
+
+// main();
+
+// let childProcess = require("child_process");
+// let p = require("path").join(__dirname, "..");
+// console.log("p = ", p);
+// let npmProcess = childProcess.spawn("npm", ["install", "lodash@latest"], { cwd: p });
+// npmProcess.stdout.on("data", (d: NodeBuffer) => console.log("on data: ", d.toString()));
+// npmProcess.stderr.on("data", (d: NodeBuffer) => console.log("on stderr data: ", d.toString()));
+// npmProcess.on("error", (d: NodeBuffer) => console.log("on error: ", d.toString()));
+// npmProcess.on("close", (d: NodeBuffer) => console.log("on close: ", d.toString()));
+
+// fiber(() => {
+// }).run();
